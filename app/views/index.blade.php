b66c3324bbb02dbfb8fd2334424fe656bee516fc
Dimas Satrio <dimassrio@outlook.com>
2014-06-24 21:16:05 +0700

    0.1 Magic Development Started
    Cloned from dimassrio/laravel-basic

---
+++ app/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout-master')
+
+@section('body')
+	
+@stop
---
+++ app/views/layout/layout-master.blade.php
@@ -0,0 +1,6 @@
+@include('layout.layout-header')
+@include('include.header')
+<div class="main-wrapper">
+	@yield('body')
+</div>
+@include('layout.layout-footer')
